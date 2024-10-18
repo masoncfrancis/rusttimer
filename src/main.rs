94a59149875f05b9987ec4bf8ad2c3de197c52a7
Mason <masoncfrancis@gmail.com>
2024-10-18 13:54:53 -0600

    Added code to hide cursor, and base cases for min and hr functions

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use std::thread;
 use std::time::Duration;
 use std::io::{self, Write};
+use crossterm::{
+    execute,
+};
 
 fn main() {
-    run_timer(5);
+    // Hide the cursor
+    match execute!(io::stdout(), crossterm::cursor::Hide) {
+        Ok(_) => (),
+        Err(e) => eprintln!("Error hiding cursor: {}", e),
+    }
+
+    let seconds: i32 = 5;
+    let minutes: i32 = 1;
+    let hours: i32 = 1;
+
+    // Run timer
+    sec_timer(5);
+    min_timer(minutes, seconds);
+    hr_timer(hours, minutes, seconds);
 }
 
-fn run_timer(seconds: i32) {
-
+fn sec_timer(seconds: i32) {
     if seconds == 0 {
         print!("{}", "\r".to_owned() + "Time's up!");
         return;
@@ -15,6 +30,20 @@ fn run_timer(seconds: i32) {
         print!("{}", "\r".to_owned() + &seconds.to_string());
         io::stdout().flush().unwrap();
         thread::sleep(Duration::from_secs(1));
-        run_timer(seconds - 1);
+        sec_timer(seconds - 1);
+    }
+}
+
+fn min_timer(minutes: i32, seconds: i32) {
+    if seconds == 0 && minutes == 0 {
+        print!("{}", "\r".to_owned() + "Time's up!");
+        return;
+    }
+}
+
+fn hr_timer(hours: i32, minutes: i32, seconds: i32) {
+    if hours == 0 && seconds == 0 && minutes == 0 {
+        print!("{}", "\r".to_owned() + "Time's up!");
+        return;
     }
 }
